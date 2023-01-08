d28c8dd7c8168cd7776e90879947400576812e13
vanloc1808 <vanloc1808@gmail.com>
2023-01-08 16:19:04 +0700

    splash screen

---
+++ app/src/main/java/com/example/datingapp/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.datingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+// https://www.youtube.com/playlist?list=PLoPTqokme6cVV_xMic2sMURVwatCfcaX4
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
