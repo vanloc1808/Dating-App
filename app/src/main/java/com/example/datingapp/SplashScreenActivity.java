1e92837310afe98842f9e3ced7badc1be854289e
vanloc1808 <vanloc1808@gmail.com>
2023-01-29 19:10:53 +0700

    rename

--- app/src/main/java/com/example/datingapp/SplashScreenActivity.java
+++ app/src/main/java/com/example/datingapp/SplashScreenActivity.java
@@ -7,17 +7,17 @@ import android.os.Bundle;
 import android.os.Handler;
 
 // https://www.youtube.com/playlist?list=PLoPTqokme6cVV_xMic2sMURVwatCfcaX4
-public class MainActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginOrRegisterActivity.class);
+                Intent intent = new Intent(SplashScreenActivity.this, LoginOrRegisterActivity.class);
                 startActivity(intent);
                 finish();
             }
