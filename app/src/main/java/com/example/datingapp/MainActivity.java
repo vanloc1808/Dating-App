4c56cbe2cf2a71ce3e4cece125b9fb1af280e525
vanloc1808 <vanloc1808@gmail.com>
2023-01-29 22:07:43 +0700

    add main activity xml

---
+++ app/src/main/java/com/example/datingapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.datingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onDislikeClicked(View view) {
+    }
+
+    public void onLikeClicked(View view) {
+
+    }
+}
